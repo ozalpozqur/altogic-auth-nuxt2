31dbf7d19cce6d511183c838da6274fccaa526f0
ozalpozqur <114391055+ozalpozqur@users.noreply.github.com>
2022-11-07 12:24:19 +0300

    Delete .nuxt directory

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['auth'] = require('../middleware/auth.js')
-middleware['auth'] = middleware['auth'].default || middleware['auth']
-
-middleware['guest'] = require('../middleware/guest.js')
-middleware['guest'] = middleware['guest'].default || middleware['guest']
-
-export default middleware
